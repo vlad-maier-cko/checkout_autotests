fad3a3d1f60fef1132a545de5f6faacfa8dc9cb1
Vlad Maier <vmaier@lohika.com>
2021-09-07 09:49:35 +0300

    Removed node_modules

--- node_modules/redoc/typings/components/Fields/FieldDetail.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-export interface FieldDetailProps {
-    value?: any;
-    label: string;
-}
-export declare class FieldDetail extends React.PureComponent<FieldDetailProps> {
-    render(): JSX.Element | null;
-}
--- node_modules/redoc/typings/components/PayloadSamples/MediaTypeSamples.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { MediaTypeModel } from '../../services/models';
-export interface PayloadSamplesProps {
-    mediaType: MediaTypeModel;
-}
-export declare class MediaTypeSamples extends React.Component<PayloadSamplesProps> {
-    render(): JSX.Element;
-}
